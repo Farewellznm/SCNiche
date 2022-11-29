dcfc08985306aa23eb7eddee25295a87bec39af1
Farewellznm <1732506718@qq.comit config --global user.name Farewellznmgit config --global user.email 1732506718@qq.com>
2022-11-29 10:48:30 +0800

    update

---
+++ R/add.R.FunctionOverview.md
@@ -0,0 +1,6 @@
+## Function Overview
+You find the list of function of this library below. For details, please use the `help()` function, or browse the [source code](). <br>
+
+
+### 1. add
+- add <- function(a,b){
---
+++ R/add.R.annot.R
@@ -0,0 +1,32 @@
+## add.R - compiled by RoxygenReady, a package by @vertesy
+
+
+#' add
+#'
+#' add <- function(a,b){
+#' @param a
+#' @param b
+#' @examples add(a =  , b =  )
+#' @export
+
+add <-function (a, b) {
+	a + b
+}
+# 让我们完成编写
+#' Returns the sum of two numbers
+#'
+#' @param a Number one.
+#' @param b Number two.
+#'
+#' @return Sum of two number.
+#' @export
+#'
+#' @examples
+#' library(rPackageTutorial)
+#' add(1, 2)
+add<-function(a,b) {
+  a+b
+}
+
+# 写完后，需要生成文档才能够真正使用
+
